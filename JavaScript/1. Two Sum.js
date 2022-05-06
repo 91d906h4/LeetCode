d83ae6dc54c7c36f8b16abf79c3790cd1f992984
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-06 15:10:53 +0900

    Create 1. Two Sum.js

---
+++ JavaScript/1. Two Sum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // Select numbers smaller than target from nums
+    for(var i = 0;i < nums.length;i++){
+        for(var j = i+1;j < nums.length;j++){
+            if(nums[i] + nums[j] == target) return [i, j];
+        }
+    }
+};
