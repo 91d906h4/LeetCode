89385ae080e48e01b5bf74fb3cdc85ca4e6e8972
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-06 15:17:10 +0900

    Update 1. Two Sum.js

--- JavaScript/1. Two Sum.js
+++ JavaScript/1. Two Sum.js
@@ -4,9 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+//     for(var i = 0;i < nums.length;i++){
+//         for(var j = i+1;j < nums.length;j++){
+//             if(nums[i] + nums[j] == target) return [i, j];
+//         }
+//     }
+
+    var a = 0;
     for(var i = 0;i < nums.length;i++){
-        for(var j = i+1;j < nums.length;j++){
-            if(nums[i] + nums[j] == target) return [i, j];
-        }
+        a = target - nums[i];
+        if(nums.includes(a) && nums.indexOf(a) != i) return [i, nums.indexOf(a)];
     }
 };
