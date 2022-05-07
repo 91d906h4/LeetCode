64bc55c1402ed69e17ab64745d091a6f508b410c
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-08 03:14:54 +0900

    Update 807. Max Increase to Keep City Skyline.py

--- Python/807. Max Increase to Keep City Skyline.py
+++ Python/807. Max Increase to Keep City Skyline.py
@@ -1,14 +1,22 @@
 import numpy
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+#         column = [max(x) for x in zip(*grid)]
+#         row = []
+#         for i in grid:
+#             row.append(max(i))
+#         width = len(column)
+#         length = len(row)
+#         result = 0
+#         for i in range(0, length):
+#             for j in range(0, width):
+#                 result = result + min(row[i], column[j]) - grid[i][j]
+#         return result
+
         column = [max(x) for x in zip(*grid)]
-        row = []
-        for i in grid:
-            row.append(max(i))
-        width = len(column)
-        length = len(row)
+        row = [max(x) for x in grid]
         result = 0
-        for i in range(0, length):
-            for j in range(0, width):
-                result = result + min(row[i], column[j]) - grid[i][j]
+        for i, c in enumerate(column):
+            for j, r in enumerate(row):
+                result = result + min(c, r) - grid[i][j]
         return result
