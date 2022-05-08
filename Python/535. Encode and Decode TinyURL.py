dd5a54e8c167cdb4bd138100084d57d34ad304f8
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-08 17:05:32 +0900

    Create 535. Encode and Decode TinyURL.py

---
+++ Python/535. Encode and Decode TinyURL.py
@@ -0,0 +1,20 @@
+import hashlib
+class Codec:
+    def __init__(self):
+        self.url ={}
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        hash_key =  "https://url.co/".join(hashlib.sha256(longUrl.encode()).hexdigest())
+        self.url[hash_key] = longUrl
+        return hash_key
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        return self.url[shortUrl]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
