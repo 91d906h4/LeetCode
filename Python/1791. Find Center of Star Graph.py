c13ac0065b46ec96b4b6838da9c433e998e53b7f
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-09 21:10:53 +0900

    Update 1791. Find Center of Star Graph.py

--- Python/1791. Find Center of Star Graph.py
+++ Python/1791. Find Center of Star Graph.py
@@ -1,9 +1,20 @@
 import numpy as np
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        a = [0] * 100001
+#         a = [0] * 100001
+#         for i in edges:
+#             a[i[0]] += 1
+#             a[i[1]] += 1
+#         return a.index(np.amax(a))
+
+        result = {}
         for i in edges:
-            a[i[0]] += 1
-            a[i[1]] += 1
-        
-        return a.index(np.amax(a))
+            if result.get(i[0]):
+                result[i[0]] += 1
+            else:
+                result[i[0]] = 1
+            if result.get(i[1]):
+                result[i[1]] += 1
+            else:
+                result[i[1]] = 1
+        return max(result, key=result.get)
