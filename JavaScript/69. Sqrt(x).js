e640341149022dc1c86be9670fb7672ba3ea00ce
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-10 17:14:05 +0900

    Update 69. Sqrt(x).js

--- JavaScript/69. Sqrt(x).js
+++ JavaScript/69. Sqrt(x).js
@@ -3,8 +3,28 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    for(var i = 1;i <= 65536;i++){
-        if(i * i == x) return i;
-        if(i * i > x) return i - 1;
+//     for(var i = 1;i <= 65536;i++){
+//         if(i * i == x) return i;
+//         if(i * i > x) return i - 1;
+//     }
+
+    let low = 1;
+    let high = Math.round(x / 2);
+    let result = 0;
+    while(low <= high){
+        let temp = low + Math.round((high - low) / 2);
+        let pow = temp * temp;
+        if(pow == x){
+            result = temp;
+            break;
+        }
+        else if(pow > x){
+            high = temp - 1;
+        }
+        else{
+            low = temp + 1;
+            result = temp;
+        }
     }
+    return result;
 };
