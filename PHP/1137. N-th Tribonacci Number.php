1a85ac834bb423733e3a4f892413afdd2410b60a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-12 20:35:53 +0900

    Create 1137. N-th Tribonacci Number.php

---
+++ PHP/1137. N-th Tribonacci Number.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function tribonacci($n) {
+        $temp_0 = 0;
+        $temp_1 = 1;
+        $temp_2 = 1;
+        if($n == 0){return 0;}
+        else if($n == 1 || $n == 2){return 1;}
+        for($i = 2;$i < $n;$i++){
+            $temp_3 = $temp_0 + $temp_1 + $temp_2;
+            $temp_0 = $temp_1;
+            $temp_1 = $temp_2;
+            $temp_2 = $temp_3;
+        }
+        return $temp_3;
+    }
+}
