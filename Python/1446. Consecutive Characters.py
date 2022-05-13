1ede99081d12db346e25382a4576ac7218e1d5d0
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-13 19:32:09 +0900

    Create 1446. Consecutive Characters.py

---
+++ Python/1446. Consecutive Characters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        temp, result = 1, 0
+        if len(s) == 1: return 1
+        for i in range(0, len(s) - 1):
+            if s[i] == s[i+1]:
+                temp += 1
+            else:
+                temp = 1
+            result = max(result, temp)
+        return result
