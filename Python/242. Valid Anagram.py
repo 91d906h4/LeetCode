f230bc04417066a28c8f313979f88b13f18c2934
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-14 18:22:08 +0900

    Update 242. Valid Anagram.py

--- Python/242. Valid Anagram.py
+++ Python/242. Valid Anagram.py
@@ -1,3 +1,6 @@
+import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+#         return sorted(s) == sorted(t)
+
+        return collections.Counter(s) == collections.Counter(t)
