194bf31f935b42641845df2153c4747bb9e5a11d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-14 14:26:21 +0900

    Update 168. Excel Sheet Column Title.py

--- Python/168. Excel Sheet Column Title.py
+++ Python/168. Excel Sheet Column Title.py
@@ -2,8 +2,8 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         result = ""
         while columnNumber > 0:
-            temp = int(columnNumber % 26)
+            temp = columnNumber % 26
             if temp == 0: temp = 26
             result = chr(temp + 64) + result
-            columnNumber = (columnNumber - temp) / 26
+            columnNumber = (columnNumber - temp) // 26
         return result
