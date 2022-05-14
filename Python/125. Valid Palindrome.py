9d89b7d9a8dd72a1a1acd90e9a0193c964ae997e
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-14 13:09:54 +0900

    Create 125. Valid Palindrome.py

---
+++ Python/125. Valid Palindrome.py
@@ -0,0 +1,13 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+#         temp1, temp2 = [], []
+#         for i in list(s):
+#             if i.isalnum():
+#                 i = i.lower()
+#                 temp1.insert(0, i)
+#                 temp2.append(i)
+#         return temp1 == temp2
+
+        temp = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        return temp == temp[::-1]
