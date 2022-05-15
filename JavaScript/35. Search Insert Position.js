e3ccfb648bc9220df972b19e7d60aa918b1bb8f5
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-15 15:31:14 +0900

    Update 35. Search Insert Position.js

--- JavaScript/35. Search Insert Position.js
+++ JavaScript/35. Search Insert Position.js
@@ -4,9 +4,19 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    nums.push(target);
-    nums = nums.sort((a, b) => a - b);
-    for(var i = 0;i < nums.length;i++){
-        if(nums[i] == target) return i;
+//     nums.push(target);
+//     nums = nums.sort((a, b) => a - b);
+//     for(var i = 0;i < nums.length;i++){
+//         if(nums[i] == target) return i;
+//     }
+
+    var high = nums.length - 1;
+    var low = 0;
+    while(high >= low){
+        var mid = ~~((high + low) / 2);
+        if(nums[mid] == target){ return mid; }
+        else if(nums[mid] > target){ high = mid - 1; }
+        else if(nums[mid] < target){ low = mid + 1; }
     }
+    return low;
 };
