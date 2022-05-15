6060830c7f86886a77b7ec2cbf767d93c08bba60
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-15 20:01:03 +0900

    Create 8. String to Integer (atoi).py

---
+++ Python/8. String to Integer (atoi).py
@@ -0,0 +1,20 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        temp = ""
+        n = 0
+        for i in list(s):
+            if (i == "-" or i == "+") and n == 0:
+                temp = temp + i
+                n += 1
+            elif i.isdigit():
+                temp = temp + i
+                n += 1
+            else:
+                break
+        if len(temp) == 0 or temp == "-" or temp == "+": return 0
+        print(temp)
+        temp = int(temp)
+        if temp > pow(2, 31) - 1: return pow(2, 31) - 1
+        elif temp < pow(2, 31) * -1: return pow(2, 31) * -1
+        else: return temp
