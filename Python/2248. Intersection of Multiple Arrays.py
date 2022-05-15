3ddd99528c2ae5fbd447dfe365c3399de448512d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-16 03:22:58 +0900

    Update 2248. Intersection of Multiple Arrays.py

--- Python/2248. Intersection of Multiple Arrays.py
+++ Python/2248. Intersection of Multiple Arrays.py
@@ -1,12 +1,16 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        result = sorted(nums[0])
-        for i in nums:
-            j = 0
-            while j < len(result):
-                k = result[j]
-                if k not in i:
-                    result.remove(k)
-                    j -= 1
-                j += 1
+#         result = sorted(nums[0])
+#         for i in nums:
+#             j = 0
+#             while j < len(result):
+#                 k = result[j]
+#                 if k not in i:
+#                     result.remove(k)
+#                     j -= 1
+#                 j += 1
+#         return sorted(result)
+
+        result = set(nums[0])
+        result = result.intersection(*nums)
         return sorted(result)
