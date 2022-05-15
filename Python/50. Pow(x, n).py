22c1840af4e8b393a9278f767e08b96f4a3331d6
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-15 22:27:12 +0900

    Update 50. Pow(x, n).py

--- Python/50. Pow(x, n).py
+++ Python/50. Pow(x, n).py
@@ -1,3 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x, n)
+#         return pow(x, n)
+#         return x ** n
+
+        if n < 0:
+            x = 1 / x
+            n = abs(n)
+        result = 1
+        while n > 0:
+            if n & 1:
+                result = result * x
+            x = x * x
+            n >>= 1
+        return result
