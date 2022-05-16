31c2183b01a89b5b6b4345e3ed495f831a0e1bc5
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-16 22:38:48 +0900

    Create 1370. Increasing Decreasing String.py

---
+++ Python/1370. Increasing Decreasing String.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        dict_ = {}
+        for i in s:
+            if i not in dict_: dict_[i] = 1
+            else: dict_[i] += 1
+        result = []
+        dict_ = sorted(dict_.items())
+        dict_ = dict((x, y) for x, y in dict_)
+        x = 0
+        while x < len(s):
+            if x % 2 == 0:
+                for i, j in enumerate(dict_):
+                    if dict_[j] > 0:
+                        result.append(j)
+                        dict_[j] -= 1
+            else:
+                for i, j in enumerate(reversed(dict_)):
+                    if dict_[j] > 0:
+                        result.append(j)
+                        dict_[j] -= 1
+            x += 1
+        return "".join(result)
