419aa2e34a4b05123e81e31412bde55ea2cb9886
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 00:48:50 +0900

    Create 657. Robot Return to Origin.py

---
+++ Python/657. Robot Return to Origin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        result = [0, 0]
+        for i in moves:
+            if i == "U": result[0] += 1
+            elif i == "D": result[0] -= 1
+            elif i == "R": result[1] += 1
+            elif i == "L": result[1] -= 1
+        return result == [0, 0]
