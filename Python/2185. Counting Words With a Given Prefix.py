742f6cee8cedb479900d01258405d692e5e4efe8
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-16 21:14:00 +0900

    Update 2185. Counting Words With a Given Prefix.py

--- Python/2185. Counting Words With a Given Prefix.py
+++ Python/2185. Counting Words With a Given Prefix.py
@@ -1,6 +1,8 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        temp = 0
-        for i in words:
-            if i.startswith(pref): temp += 1
-        return temp
+#         temp = 0
+#         for i in words:
+#             if i.startswith(pref): temp += 1
+#         return temp
+
+        return sum([1 for x in words if x.startswith(pref)])
