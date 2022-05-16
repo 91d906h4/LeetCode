7b64ecaeb6d7156a8e3b97d6b3f914635ffdf95d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 00:50:46 +0900

    Update 657. Robot Return to Origin.py

--- Python/657. Robot Return to Origin.py
+++ Python/657. Robot Return to Origin.py
@@ -1,9 +1,11 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        result = [0, 0]
-        for i in moves:
-            if i == "U": result[0] += 1
-            elif i == "D": result[0] -= 1
-            elif i == "R": result[1] += 1
-            elif i == "L": result[1] -= 1
-        return result == [0, 0]
+#         result = [0, 0]
+#         for i in moves:
+#             if i == "U": result[0] += 1
+#             elif i == "D": result[0] -= 1
+#             elif i == "R": result[1] += 1
+#             elif i == "L": result[1] -= 1
+#         return result == [0, 0]
+
+        return moves.count("U") == moves.count("D") and moves.count("R") == moves.count("L")
