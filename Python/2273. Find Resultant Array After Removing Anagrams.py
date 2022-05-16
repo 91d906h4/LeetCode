2040fdfd0608b6fb0b3368b4f683d87896564e50
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-16 12:38:55 +0900

    Create 2273. Find Resultant Array After Removing Anagrams.py

---
+++ Python/2273. Find Resultant Array After Removing Anagrams.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        result = []
+        counter = [-1]
+        t = 0
+        for i in words:
+            temp = 1
+            for j in i:
+                temp *= (ord(j) - ord('/'))
+            if temp not in counter:
+                counter.append(temp)
+                result.append(i)
+                counter.pop(0)
+        return result
