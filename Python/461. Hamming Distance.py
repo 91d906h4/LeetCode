0ba8c506df7721e1a78547d8a16054a4bcb04d62
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 01:14:53 +0900

    Update 461. Hamming Distance.py

--- Python/461. Hamming Distance.py
+++ Python/461. Hamming Distance.py
@@ -1,6 +1,8 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        temp = 0
-        for i, j in zip_longest(reversed(str(bin(x))[2:]), reversed(str(bin(y))[2:]), fillvalue = 0):
-            if str(i) != str(j): temp += 1
-        return temp
+#         temp = 0
+#         for i, j in zip_longest(reversed(str(bin(x))[2:]), reversed(str(bin(y))[2:]), fillvalue = 0):
+#             if str(i) != str(j): temp += 1
+#         return temp
+
+        return (x ^ y).bit_count()
