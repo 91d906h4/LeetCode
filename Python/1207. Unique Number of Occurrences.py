c47ba026a3b6c65e5006d0393e3755993d5c47dc
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 21:11:35 +0900

    Update 1207. Unique Number of Occurrences.py

--- Python/1207. Unique Number of Occurrences.py
+++ Python/1207. Unique Number of Occurrences.py
@@ -1,10 +1,13 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        dict_ = {}
-        for i in arr:
-            dict_[i] = dict_.get(i, 0) + 1
-        temp = {}
-        for _, i in dict_.items():
-            if i not in temp: temp[i] = 0
-            else: return False
-        return True
+#         dict_ = {}
+#         for i in arr:
+#             dict_[i] = dict_.get(i, 0) + 1
+#         temp = {}
+#         for _, i in dict_.items():
+#             if i not in temp: temp[i] = 0
+#             else: return False
+#         return True
+
+        temp = Counter(arr).values()
+        return len(set(temp)) == len(temp)
