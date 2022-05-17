258ccae8a2351105c76a03e05a92e2ed0a93fc08
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 18:37:33 +0900

    Create 2032. Two Out of Three.py

---
+++ Python/2032. Two Out of Three.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        nums1 = list(dict.fromkeys(nums1))
+        nums2 = list(dict.fromkeys(nums2))
+        nums3 = list(dict.fromkeys(nums3))
+        
+        dict_ = {}
+        for i, j, k in zip_longest(nums1, nums2, nums3, fillvalue = 0):
+            if i not in dict_: dict_[i] = 1
+            else: dict_[i] += 1
+            if j not in dict_: dict_[j] = 1
+            else: dict_[j] += 1
+            if k not in dict_: dict_[k] = 1
+            else: dict_[k] += 1
+        
+        return sorted([x for x, y in dict_.items() if y >= 2 and x != 0])
