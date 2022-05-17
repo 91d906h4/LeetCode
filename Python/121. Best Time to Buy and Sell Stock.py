0b82389e3c3ec7542023679e70bd94b701d7c8ab
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-18 01:14:14 +0900

    Update 121. Best Time to Buy and Sell Stock.py

--- Python/121. Best Time to Buy and Sell Stock.py
+++ Python/121. Best Time to Buy and Sell Stock.py
@@ -4,12 +4,19 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        size = len(prices)
-        xmin = 10001
-        xresult = 0
-        i = 0
-        while i < size:
-            xresult = max(xresult, prices[i] - xmin)
-            xmin = min(xmin, prices[i])
-            i += 1
-        return xresult
+#         size = len(prices)
+#         xmin = 10001
+#         xresult = 0
+#         i = 0
+#         while i < size:
+#             xresult = max(xresult, prices[i] - xmin)
+#             xmin = min(xmin, prices[i])
+#             i += 1
+#         return xresult
+
+        xmin = prices[0]
+        result = 0
+        for i in prices:
+            xmin = min(xmin, i)
+            result = max(result, i - xmin)
+        return result
