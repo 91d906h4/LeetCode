a6d4defa95a6efdd3b85f2ae8d6ef611e0d33ed1
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 20:11:41 +0900

    Create 2053. Kth Distinct String in an Array.py

---
+++ Python/2053. Kth Distinct String in an Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        dict_ = {}
+        for i in arr:
+            if i not in dict_: dict_[i] = 1
+            else: dict_[i] += 1
+        temp = [x for x, y in dict_.items() if y == 1]
+        if len(temp) >= k: return temp[k-1]
+        else: return ""
