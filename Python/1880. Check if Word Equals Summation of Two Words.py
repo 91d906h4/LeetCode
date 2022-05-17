518622ba511ab0e836db1b6ded39e8ebec121448
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-17 12:41:57 +0900

    Update 1880. Check if Word Equals Summation of Two Words.py

--- Python/1880. Check if Word Equals Summation of Two Words.py
+++ Python/1880. Check if Word Equals Summation of Two Words.py
@@ -1,8 +1,14 @@
 class Solution:
     def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+#         def strmun(s: str) -> int:
+#             temp = ""
+#             for i in list(s):
+#                 temp = temp + str(ord(i) - 97)
+#             return int(temp)
+
         def strmun(s: str) -> int:
-            temp = ""
-            for i in list(s):
-                temp = temp + str(ord(i) - 97)
-            return int(temp)
+            temp = 0
+            for i in s:
+                temp = temp * 10 + ord(i) - 97
+            return temp
         return strmun(firstWord) + strmun(secondWord) == strmun(targetWord)
