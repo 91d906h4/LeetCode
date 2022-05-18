7b01206b50aba61d9674b5be7f4163b266294a2d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-18 17:20:30 +0900

    Create 1385. Find the Distance Value Between Two Arrays.py

---
+++ Python/1385. Find the Distance Value Between Two Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        len_ = len(arr1)
+        for i in arr1:
+            for j in arr2:
+                if abs(i - j) <= d:
+                    len_ -= 1
+                    break
+        return len_
