eeb1f439f9b2bb76f4ac573607ed19058012b346
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-18 20:02:31 +0900

    Update 566. Reshape the Matrix.py

--- Python/566. Reshape the Matrix.py
+++ Python/566. Reshape the Matrix.py
@@ -7,10 +7,16 @@ class Solution:
                 mat_t.append(j)
         result = []
         x = 0
-        for i in range(r):
-            temp = []
-            for j in range(c):
-                temp.append(mat_t[x])
-                x += 1
-            result.append(temp)
+
+#         for i in range(r):
+#             temp = []
+#             for j in range(c):
+#                 temp.append(mat_t[x])
+#                 x += 1
+#             result.append(temp)
+
+        for i, element in enumerate(mat_t):
+            if i % c == 0: result.append([])
+            result[-1].append(element)
+
         return result
