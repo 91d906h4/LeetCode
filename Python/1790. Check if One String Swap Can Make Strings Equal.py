7dde1b0c7b48d09f7e4472cde1a8d2476116ad6f
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-18 21:09:24 +0900

    Update 1790. Check if One String Swap Can Make Strings Equal.py

--- Python/1790. Check if One String Swap Can Make Strings Equal.py
+++ Python/1790. Check if One String Swap Can Make Strings Equal.py
@@ -1,9 +1,17 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        size = len(s1)
-        for i in range(size):
-            for j in range(i, size):
-                temp = list(s1)
-                temp[i], temp[j] = temp[j], temp[i]
-                if "".join(temp) == s2: return True
-        return False
+#         size = len(s1)
+#         for i in range(size):
+#             for j in range(i, size):
+#                 temp = list(s1)
+#                 temp[i], temp[j] = temp[j], temp[i]
+#                 if "".join(temp) == s2: return True
+#         return False
+
+        temp = [i for i in range(len(s1)) if s1[i] != s2[i]]
+        len_ = len(temp)
+        if len_ == 0: return True
+        elif len_ == 2:
+            if s1[temp[0]] == s2[temp[1]] and s1[temp[1]] == s2[temp[0]]: return True
+            else: return False
+        else: return False
