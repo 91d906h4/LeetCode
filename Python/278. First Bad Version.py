bff2c866cffe2758d554afa860b6315a71549336
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-19 14:42:13 +0900

    Update 278. First Bad Version.py

--- Python/278. First Bad Version.py
+++ Python/278. First Bad Version.py
@@ -3,9 +3,20 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
+#         high, low = n, 1
+#         while high > low:
+#             mid = (high + low) // 2
+#             if isBadVersion(mid): high = mid
+#             else: low = mid + 1
+#         return low
+
         high, low = n, 1
-        while high > low:
+        while high >= low:
             mid = (high + low) // 2
-            if isBadVersion(mid): high = mid
-            else: low = mid + 1
+            if isBadVersion(mid) and not isBadVersion(mid - 1):
+                return mid
+            elif isBadVersion(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
         return low
