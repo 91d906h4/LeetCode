b0d3c0a2b48e2b220c619ed99fac52705d4b1972
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-19 21:50:13 +0900

    Update 414. Third Maximum Number.py

--- Python/414. Third Maximum Number.py
+++ Python/414. Third Maximum Number.py
@@ -1,5 +1,8 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+#         if len(set(nums)) < 3: return max(nums)
+#         else: return sorted(set(nums), reverse = True)[2]
+
         temp = set(nums)
         temp = list(temp)
         temp.sort(reverse = True)
