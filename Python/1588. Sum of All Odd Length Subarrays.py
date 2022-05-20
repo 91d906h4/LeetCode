ca52646d1f19fe874fc04c91c79bfe90f64c0089
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-21 02:39:49 +0900

    Update 1588. Sum of All Odd Length Subarrays.py

--- Python/1588. Sum of All Odd Length Subarrays.py
+++ Python/1588. Sum of All Odd Length Subarrays.py
@@ -1,12 +1,12 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        # len_ = len(arr)
-        # result = []
-        # for i in range(len_):
-        #     for j in range(i, len_):
-        #         if (i + j) % 2 == 0:
-        #             result += [arr[i:j + 1]]
-        # return sum([sum(x) for x in result])
+#         result = []
+#         len_ = len(arr)
+#         for i in range(len_):
+#             for j in range(i, len_):
+#                 if (i + j) % 2 == 0:
+#                     result += arr[i:j + 1]
+#         return sum(result)
 
         len_ = len(arr)
         result = 0
