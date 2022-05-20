6d2a96c9a5e3344745bf8a46ae8c702113ba77db
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-20 14:11:27 +0900

    Update 283. Move Zeroes.py

--- Python/283. Move Zeroes.py
+++ Python/283. Move Zeroes.py
@@ -4,12 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        size = len(nums)
+#         size = len(nums)
+#         i, j = 0, 0
+#         while j < size:
+#             if(nums[i] == 0):
+#                 nums.pop(i)
+#                 nums.append(0)
+#                 i -= 1
+#             i += 1
+#             j += 1
+
         i, j = 0, 0
-        while j < size:
-            if(nums[i] == 0):
+        for j in range(len(nums)):
+            if nums[i] == 0:
                 nums.pop(i)
                 nums.append(0)
-                i -= 1
-            i += 1
+            else:
+                i += 1
             j += 1
