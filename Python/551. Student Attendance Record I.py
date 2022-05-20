fb978e6212522032892682ce087f55139349eedb
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-20 19:02:11 +0900

    Update 551. Student Attendance Record I.py

--- Python/551. Student Attendance Record I.py
+++ Python/551. Student Attendance Record I.py
@@ -1,13 +1,15 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        A, L, temp = 0, 0, 0
-        for i in s:
-            if i == "P":
-                temp = 0
-            elif i == "A":
-                A += 1
-                temp = 0
-            elif i == "L":
-                temp += 1
-            L = max(L, temp)
-        return A < 2 and L < 3
+#         A, L, temp = 0, 0, 0
+#         for i in s:
+#             if i == "P":
+#                 temp = 0
+#             elif i == "A":
+#                 A += 1
+#                 temp = 0
+#             elif i == "L":
+#                 temp += 1
+#             L = max(L, temp)
+#         return A < 2 and L < 3
+
+        return s.count("A") < 2 and "LLL" not in s
