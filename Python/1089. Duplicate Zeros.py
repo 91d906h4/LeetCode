49a7d07011d9f87db9961f048bc321e83b228055
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-21 14:38:53 +0900

    Update 1089. Duplicate Zeros.py

--- Python/1089. Duplicate Zeros.py
+++ Python/1089. Duplicate Zeros.py
@@ -4,12 +4,11 @@ class Solution:
         Do not return anything, modify arr in-place instead.
         """
         len_ = len(arr)
-        i, j = 0, 0
+        j = 0
         for i in range(len_):
             if arr[j] == 0:
                 arr.insert(j, 0)
                 j += 1
-            i += 1
             j += 1
         arr[:] = arr[:len_]
         return arr
