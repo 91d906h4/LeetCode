64aee6926ef3a73a17bb9898973acd8d6785df2d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-23 17:36:31 +0900

    Update 1017. Convert to Base -2.py

--- Python/1017. Convert to Base -2.py
+++ Python/1017. Convert to Base -2.py
@@ -1,15 +1,10 @@
 class Solution:
     def baseNeg2(self, n: int) -> str:
-        def n_base_k(n, k):
-            if n == 0:
-                return "0"
-            else:
-                result = []
-                while n != 0:
-                    n, i = divmod(n, k)
-                    if i < 0:
-                        n, i = n + 1, i + 2
-                    result.append(str(i))
-                return "".join(result)[::-1]
-        
-        return n_base_k(n, -2)
+        if n == 0: return "0"
+        result = []
+        while n != 0:
+            n, i = divmod(n, -2)
+            if i < 0:
+                n, i = n + 1, i + 2
+            result.append(str(i))
+        return "".join(result)[::-1]
