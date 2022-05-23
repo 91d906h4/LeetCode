3ab9fb0997bed01ecb93182956a2153fd69f89e9
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-23 16:49:00 +0900

    Update 973. K Closest Points to Origin.py

--- Python/973. K Closest Points to Origin.py
+++ Python/973. K Closest Points to Origin.py
@@ -1,3 +1,5 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return sorted(points, key = lambda a: sqrt(pow(a[0], 2) + pow(a[1], 2)))[:k]
+#         return sorted(points, key = lambda a: sqrt(pow(a[0], 2) + pow(a[1], 2)))[:k]
+
+        return sorted(points, key = lambda a: pow(a[0], 2) + pow(a[1], 2))[:k]
