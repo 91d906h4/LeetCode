96715155d8c87627fb6ef583962f15111afad930
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-23 13:57:58 +0900

    Update 343. Integer Break.py

--- Python/343. Integer Break.py
+++ Python/343. Integer Break.py
@@ -1,8 +1,10 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        if n < 4: return n - 1
-        result = 1
-        while n > 4:
-            result *= 3
-            n -= 3
-        return result * n
+#         if n < 4: return n - 1
+#         result = 1
+#         while n > 4:
+#             result *= 3
+#             n -= 3
+#         return result * n
+
+        return n - 1 if n < 4 else pow(3, (n - 2) // 3) * ((n - 2) % 3 + 2)
