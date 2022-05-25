59016a9a548d24096ea592d924d115da99ee2ed1
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-26 03:25:05 +0900

    Update 1009. Complement of Base 10 Integer.py

--- Python/1009. Complement of Base 10 Integer.py
+++ Python/1009. Complement of Base 10 Integer.py
@@ -1,3 +1,7 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
-        return int("".join(["1" if x == "0" else "0" for x in list(str(bin(n)))[2:]]), 2)
+#         return int("".join(["1" if x == "0" else "0" for x in list(str(bin(n)))[2:]]), 2)
+
+        k = 1
+        while k < n: k = k * 2 + 1
+        return n ^ k
