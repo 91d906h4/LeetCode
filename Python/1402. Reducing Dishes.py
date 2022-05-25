5202afbb89977c223e7112daeee255341387ce4c
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-25 17:51:48 +0900

    Update 1402. Reducing Dishes.py

--- Python/1402. Reducing Dishes.py
+++ Python/1402. Reducing Dishes.py
@@ -1,10 +1,20 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
+#         satisfaction.sort(reverse = True)
+#         max_ = 0
+#         for i in range(len(satisfaction) + 1):
+#             temp = 0
+#             for j, k in enumerate(satisfaction[:i][::-1]):
+#                 temp += (j + 1) * k
+#             if temp > max_: max_ = temp
+#         return max_
+
         satisfaction.sort(reverse = True)
-        max_ = 0
-        for i in range(len(satisfaction) + 1):
-            temp = 0
-            for j, k in enumerate(satisfaction[:i][::-1]):
-                temp += (j + 1) * k
-            if temp > max_: max_ = temp
-        return max_
+        temp, result = 0, 0
+        for i in satisfaction:
+            temp += i
+            if temp > 0:
+                result += temp
+            else:
+                break
+        return result
