aaa001e94548cf0553ff62421addfca0179d63c7
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-05-26 21:30:07 +0900

    Update 153. Find Minimum in Rotated Sorted Array.py

--- Python/153. Find Minimum in Rotated Sorted Array.py
+++ Python/153. Find Minimum in Rotated Sorted Array.py
@@ -3,6 +3,6 @@ class Solution:
         l, r = 0, len(nums) - 1
         while nums[l] > nums[r]:
             m = (l + r) // 2
-            if nums[m] > nums[l]: l = m + 1
+            if nums[m] > nums[r]: l = m + 1
             else: r = m
         return nums[l]
