eac7fdf078d66396a64753920c4926009df61902
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-01 16:08:16 +0900

    Update 260. Single Number III.py

--- Python/260. Single Number III.py
+++ Python/260. Single Number III.py
@@ -1,3 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
 #         return [x[0] for x in Counter(nums).items() if x[1] == 1]
+
+#         hoge = []
+#         for i in nums:
+#             if i not in hoge: hoge.append(i)
+#             else: hoge.remove(i)
+#         return hoge
+        
+        hoge = Counter()
+        fuga = []
+        for i in nums:
+            hoge[i] += 1
+        for i, j in hoge.items():
+            if j == 1: fuga.append(i)
+        return fuga
