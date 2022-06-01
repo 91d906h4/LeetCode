618b008319727c632c1ad26de3488ef0d7144a31
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-02 02:33:40 +0900

    Delete 167. Two Sum II - Input Array Is Sorted.ts

--- TypeScript/167. Two Sum II - Input Array Is Sorted.ts
+++
@@ -1,8 +0,0 @@
-function twoSum(numbers: number[], target: number): number[] {
-    var a = numbers.length - 1, b = 0;
-    while(numbers[a] + numbers[b] != target){
-        if(numbers[a] + numbers[b] > target){ a--; }
-        else{ b++; }
-    }
-    return [b + 1, a + 1];
-};
