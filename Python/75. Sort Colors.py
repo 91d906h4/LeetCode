4e0bfcee72606d070d280264d48145d976502fdb
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-03 21:28:15 +0900

    Update 75. Sort Colors.py

--- Python/75. Sort Colors.py
+++ Python/75. Sort Colors.py
@@ -4,6 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+#         hoge, fuga = [], 0
+#         for i in nums:
+#             if i == 0:
+#                 hoge.insert(0, i)
+#                 fuga += 1
+#             elif i == 2:
+#                 hoge.append(i)
+#             else:
+#                 hoge.insert(fuga, i)
+#         for i in range(len(nums)):
+#             nums[i] = hoge[i]
+
         size = len(nums)
         output = [0] * size
         count = [0] * 3
@@ -18,4 +30,3 @@ class Solution(object):
             i -= 1
         for i in range(0, size):
             nums[i] = output[i]
-        return nums
