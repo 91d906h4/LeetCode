7041ba7d58a578056ff4b9718cff1d87d30a8059
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-11 20:27:48 +0900

    Update 344. Reverse String.py

--- Python/344. Reverse String.py
+++ Python/344. Reverse String.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i, j = 0, len(s) - 1
-        while i <= j:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+#         i, j = 0, len(s) - 1
+#         while i <= j:
+#             s[i], s[j] = s[j], s[i]
+#             i += 1
+#             j -= 1
+
+        s.reverse()
