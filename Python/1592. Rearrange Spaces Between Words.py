40fc1d8d3e4edfa5d9bc36528cf9040407efa400
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-21 20:40:53 +0900

    Update 1592. Rearrange Spaces Between Words.py

--- Python/1592. Rearrange Spaces Between Words.py
+++ Python/1592. Rearrange Spaces Between Words.py
@@ -1,8 +1,10 @@
 class Solution:
     def reorderSpaces(self, text: str) -> str:
-        s = text.count(" ")
-        text = text.split()
-        temp = len(text) - 1
-        n = s // (temp) if temp > 0 else 0
-        m = s % (temp) if temp > 0 else s
-        return (" " * n).join(text) + " " * m
+#         s = text.count(" ")
+#         text = text.split()
+#         temp = len(text) - 1
+#         n = s // temp if temp > 0 else 0
+#         m = s % temp if temp > 0 else s
+#         return (" " * n).join(text) + " " * m
+
+        return (" " * (text.count(" ") // (len(text.split()) - 1) if len(text.split()) - 1 > 0 else 0)).join(text.split()) + " " * (text.count(" ") % (len(text.split()) - 1) if len(text.split()) - 1 > 0 else text.count(" "))
