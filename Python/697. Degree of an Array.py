180824fe6b80afeb89d9b3adc553ca4f8c5e5d2e
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-07-07 20:15:50 +0900

    Update 697. Degree of an Array.py

--- Python/697. Degree of an Array.py
+++ Python/697. Degree of an Array.py
@@ -1,11 +1,11 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
-        hoge = Counter(nums)
-        hoge = sorted(hoge.items(), key = lambda item: item[1], reverse = True)
+        hoge = sorted(Counter(nums).items(), key = lambda item: item[1], reverse = True)
         fuga = [x[0] for x in hoge if x[1] == hoge[0][1]]
         m = len(nums)
+        n = m
         temp = nums[::-1]
         for i in fuga:
-            m = min(m, len(nums) - temp.index(i) - nums.index(i))
+            m = min(m, n - temp.index(i) - nums.index(i))
             if m == 2: break
         return m
