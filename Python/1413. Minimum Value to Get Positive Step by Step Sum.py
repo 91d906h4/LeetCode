d1a917ba79024f596f062182efab14709a250daa
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-07-26 12:10:37 +0900

    Update 1413. Minimum Value to Get Positive Step by Step Sum.py

--- Python/1413. Minimum Value to Get Positive Step by Step Sum.py
+++ Python/1413. Minimum Value to Get Positive Step by Step Sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        min_ = float("inf")
-        temp = 0
+        min_, temp = float("inf"), 0
         for i in nums:
             min_ = min(min_, temp := temp + i)
         return abs(min_) + 1 if min_ < 1 else 1
