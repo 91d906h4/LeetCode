7ee45ac9b10cd69017e38845f2eb5d0814f77692
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-07-28 16:30:09 +0900

    Update 2240. Number of Ways to Buy Pens and Pencils.py

--- Python/2240. Number of Ways to Buy Pens and Pencils.py
+++ Python/2240. Number of Ways to Buy Pens and Pencils.py
@@ -1,7 +1,9 @@
 class Solution:
     def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
-        result = 0
-        for i in range(total // cost1 + 1):
-            temp = (total - i * cost1) // cost2
-            result += temp + 1
-        return result
+#         result = 0
+#         for i in range(total // cost1 + 1):
+#             temp = (total - i * cost1) // cost2
+#             result += temp + 1
+#         return result
+
+        return sum((total - i * cost1) // cost2 + 1 for i in range(total // cost1 + 1))
