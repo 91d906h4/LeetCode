af6311f75f75c2e290c17b92fd6b6238083400ff
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-07-28 16:26:46 +0900

    Create 2240. Number of Ways to Buy Pens and Pencils.py

---
+++ Python/2240. Number of Ways to Buy Pens and Pencils.py
@@ -0,0 +1,7 @@
+class Solution:
+    def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
+        result = 0
+        for i in range(total // cost1 + 1):
+            temp = (total - i * cost1) // cost2
+            result += temp + 1
+        return result
