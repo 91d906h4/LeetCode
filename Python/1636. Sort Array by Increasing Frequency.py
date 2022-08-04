159a64f1437b330226fc94732845efc6a87a526f
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-08-04 15:55:07 +0900

    Create 1636. Sort Array by Increasing Frequency.py

---
+++ Python/1636. Sort Array by Increasing Frequency.py
@@ -0,0 +1,7 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        temp = sorted(Counter(nums).items(), key = lambda x: (x[1], x[0] * -1))
+        r = []
+        for i, j in temp:
+            r += [i] * j
+        return r
