c9249f6c1abf14853c69be2c0a957270e3219a3a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-08-05 13:49:23 +0900

    Update 1528. Shuffle String.java

--- Java/1528. Shuffle String.java
+++ Java/1528. Shuffle String.java
@@ -1,9 +1,15 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        String[] r = new String[s.length()];
-        for(int i = 0; i< s.length(); i++){
-            r[indices[i]] = String.valueOf(s.charAt(i));
+//         String[] r = new String[s.length()];
+//         for(int i = 0; i< s.length(); i++){
+//             r[indices[i]] = String.valueOf(s.charAt(i));
+//         }
+//         return String.join("", r);
+
+        StringBuilder r = new StringBuilder(s);
+        for(int i = 0; i < s.length(); i++){
+            r.setCharAt(indices[i], s.charAt(i));
         }
-        return String.join("", r);
+        return r.toString();
     }
 }
