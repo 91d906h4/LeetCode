2f5b727f1508f2c9450d79ce46e0ff90f45384ca
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-08-08 20:46:34 +0900

    Update 746. Min Cost Climbing Stairs.py

--- Python/746. Min Cost Climbing Stairs.py
+++ Python/746. Min Cost Climbing Stairs.py
@@ -1,7 +1,15 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         for i in range(2, len(cost)):
+#             cost[i] += min(cost[i - 1], cost[i - 2])
+#             print(cost)
+#         l = len(cost)
+#         return min(cost[l - 1], cost[l - 2])
+
+        
+        m, n = cost[0], cost[1]
+        t = m
         for i in range(2, len(cost)):
-            cost[i] += min(cost[i - 1], cost[i - 2])
-            print(cost)
-        l = len(cost)
-        return min(cost[l - 1], cost[l - 2])
+            t = min(m, n) + cost[i]
+            m, n = n, t
+        return min(m, n)
