3447be13df9978b5d3b19c3584dcdf205d9c238b
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-08-30 17:10:47 +0900

    Update 2363. Merge Similar Items.py

--- Python/2363. Merge Similar Items.py
+++ Python/2363. Merge Similar Items.py
@@ -1,8 +1,6 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
         t = Counter()
-        for i, j in items1:
-            t[i] += j
-        for i, j in items2:
+        for i, j in items1 + items2:
             t[i] += j
         return sorted(t.items())
