e16d00ab7a3d303d1d715b5c23cb22cd9c284e50
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-11-08 13:17:25 +0900

    Update 912. Sort an Array.py

--- Python/912. Sort an Array.py
+++ Python/912. Sort an Array.py
@@ -1,3 +1,19 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         return sorted(nums)
+
+#         Bobble Sort
+#         l = len(nums)
+#         for i in range(l):
+#             for j in range(l - i - 1):
+#                 if nums[j] > nums[j + 1]:
+#                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
+#         return nums
+
+#         Selection Sort
+#         l = len(nums)
+#         for i in range(l):
+#             for j in range(i + 1, l):
+#                 if nums[j] < nums[i]:
+#                     nums[i], nums[j] = nums[j], nums[i]
+#         return nums
