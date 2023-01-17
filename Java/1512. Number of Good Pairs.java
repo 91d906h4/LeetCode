9477570c3afff2dd61b7fde3dbad4445cf2d9be0
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-01-17 23:02:55 +0900

    Create 1512. Number of Good Pairs.java

---
+++ Java/1512. Number of Good Pairs.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int temp[] = new int[101], ans = 0;
+
+        for(int i: nums) temp[i]++;
+        for(int i: temp) ans += i * (i - 1) / 2;
+
+        return ans;
+    }
+}
