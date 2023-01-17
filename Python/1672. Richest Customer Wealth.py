0cee2fdb2d3e6ec5abdedb2d79668993721ce780
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-01-17 23:09:37 +0900

    Update 1672. Richest Customer Wealth.py

--- Python/1672. Richest Customer Wealth.py
+++ Python/1672. Richest Customer Wealth.py
@@ -1,6 +1,9 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        max_ = -1
-        for i in accounts:
-            max_ = max(max_, sum(i))
-        return max_
+#         max_ = -1
+#         for i in accounts:
+#             max_ = max(max_, sum(i))
+#         return max_
+
+
+        return max(map(sum, accounts))
