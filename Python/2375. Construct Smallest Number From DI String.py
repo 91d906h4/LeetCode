b03e32ec66d8a32d2ff4f3d384c1b642745e5c4c
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-01-19 15:07:27 +0900

    Create 2375. Construct Smallest Number From DI String.py

---
+++ Python/2375. Construct Smallest Number From DI String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        l = len(pattern)
+        for i in permutations(range(1, l + 2)):
+            for j, p in enumerate(pattern):
+                if p == 'I' and not i[j] < i[j + 1]: break
+                if p == 'D' and not i[j] > i[j + 1]: break
+                if j == l - 1: return ''.join(map(str, i))
+        
+        return ''
