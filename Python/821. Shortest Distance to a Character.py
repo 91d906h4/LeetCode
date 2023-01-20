e53de5f0ed54750f9dc4d59701cbf2a759398edf
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-01-20 14:30:46 +0900

    Update 821. Shortest Distance to a Character.py

--- Python/821. Shortest Distance to a Character.py
+++ Python/821. Shortest Distance to a Character.py
@@ -4,10 +4,10 @@ class Solution:
 
         for i, x in enumerate(s):
             if x == c: temp.append(i)
-        
+
         for i, x in enumerate(s):
             m = 1001
             for j in temp: m = min(m, abs(i - j))
             res.append(m)
-        
+
         return res
