42cc5996980162c34402f8ee90007dc9fe16acf7
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-01-20 22:44:57 +0900

    Update 2309. Greatest English Letter in Upper and Lower Case.py

--- Python/2309. Greatest English Letter in Upper and Lower Case.py
+++ Python/2309. Greatest English Letter in Upper and Lower Case.py
@@ -1,8 +1,13 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
-        s, m = set(s), ' '
+#         s, m = set(s), ' '
 
-        for i in s:
-            if chr(ord(i) + 32) in s and ord(i) > ord(m): m = i
+#         for i in s:
+#             if chr(ord(i) + 32) in s and ord(i) > ord(m): m = i
         
-        return m if m != ' ' else ''
+#         return m if m != ' ' else ''
+
+        for i in range(90, 64, -1):
+            if chr(i) in s and chr(i + 32) in s: return chr(i)
+        
+        return ''
