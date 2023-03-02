a11f7b5d7230fd06362b5a2c3d92e89970d30d34
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-03-02 16:25:46 +0900

    Create 1108. Defanging an IP Address.cpp

---
+++ C++/1108. Defanging an IP Address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res = "";
+
+        for(char i: address){
+            if(i == '.') res += "[.]";
+            else res += i;
+        }
+
+        return res;
+    }
+};
