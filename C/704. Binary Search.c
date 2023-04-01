ab31430786bd18609c8c7ea240cc0a387d513a24
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-04-01 15:44:09 +0900

    Create 704. Binary Search.c

---
+++ C/704. Binary Search.c
@@ -0,0 +1,13 @@
+int search(int* nums, int numsSize, int target){
+    int l = 0, r = numsSize - 1;
+
+    while (l <= r) {
+        int m = (l + r) / 2;
+
+        if (*(nums + m) == target) return m;
+        else if (*(nums + m) > target) r = m - 1;
+        else l = m + 1;
+    }
+
+    return -1;
+}
