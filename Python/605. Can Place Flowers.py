7d4e102d21f4debbb8835389cb449388b9d95d4a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-04-06 15:54:44 +0900

    Update 605. Can Place Flowers.py

--- Python/605. Can Place Flowers.py
+++ Python/605. Can Place Flowers.py
@@ -1,9 +1,18 @@
 class Solution:
     def canPlaceFlowers(self, flowered: List[int], n: int) -> bool:
-        for i, element in enumerate(flowered):
-            if element == 0 and (i == 0 or flowered[i - 1] == 0) and (i == len(flowered) - 1 or flowered[i + 1] == 0):
-                flowered[i] = 1
+#         for i, element in enumerate(flowered):
+#             if element == 0 and (i == 0 or flowered[i - 1] == 0) and (i == len(flowered) - 1 or flowered[i + 1] == 0):
+#                 flowered[i] = 1
+#                 n -= 1
+#             if n <= 0:
+#                 return True
+#         return False
+
+        flowerbed = [0] + flowerbed + [0]
+
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i - 1] == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
                 n -= 1
-            if n <= 0:
-                return True
-        return False
+
+        return n <= 0
