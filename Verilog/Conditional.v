f67730b54f4be04ff8450c44dae5cba98fa3e9a6
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 01:05:52 +0900

    Create Conditional.v

---
+++ Verilog/Conditional.v
@@ -0,0 +1,13 @@
+// https://hdlbits.01xz.net/wiki/Conditional
+
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+
+    // assign intermediate_result1 = compare? true: false;
+    wire [7:0] t1, t2;
+    assign t1 = (a < b) ? a : b;
+    assign t2 = (c < d) ? c : d;
+    assign min = (t1 < t2) ? t1 : t2;
+
+endmodule
