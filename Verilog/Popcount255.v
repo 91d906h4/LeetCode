53c902285dd85f66dec64aa70202c274ee35d142
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 01:16:44 +0900

    Create Popcount255.v

---
+++ Verilog/Popcount255.v
@@ -0,0 +1,19 @@
+// https://hdlbits.01xz.net/wiki/Popcount255
+
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    
+    integer i;
+    reg [7:0] counter;
+    
+    always @(*) begin
+        counter = 0;
+        for (i = 0; i < 255; i++) begin
+            if (in[i] == 1'b1) counter = counter + 1'b1;
+        end
+    end
+    
+    assign out = counter;
+
+endmodule
