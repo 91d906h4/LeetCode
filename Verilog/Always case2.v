fd7c0d3bd2a2b2475afa7b0c22328acb1674055c
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 00:38:59 +0900

    Create Always case2.v

---
+++ Verilog/Always case2.v
@@ -0,0 +1,18 @@
+// https://hdlbits.01xz.net/wiki/Always_case2
+
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+
+    always @(*) begin
+        casez (in[3:0])
+            4'bzzz1: pos = 0;
+            4'bzz1z: pos = 1;
+            4'bz1zz: pos = 2;
+            4'b1zzz: pos = 3;
+            default: pos = 0;
+        endcase
+    end
+    
+endmodule
