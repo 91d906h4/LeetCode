d28e589fe164e34b843e2d609a8f862229f266c5
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-01 23:26:51 +0900

    Update Module shift.v

--- Verilog/Module shift.v
+++ Verilog/Module shift.v
@@ -4,7 +4,7 @@ module top_module ( input clk, input d, output q );
     wire w1, w2;
 
     my_dff instance_1(clk, d, w1),
-    	     instance_2(clk, w1, w2),
-    	     instance_3(clk, w2, q);
+    	   instance_2(clk, w1, w2),
+    	   instance_3(clk, w2, q);
 
 endmodule
