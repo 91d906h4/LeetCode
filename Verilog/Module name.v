fa2ab86af777e55c54a175654e1824195dd9e4d9
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-01 23:22:20 +0900

    Create Module name.v

---
+++ Verilog/Module name.v
@@ -0,0 +1,14 @@
+// https://hdlbits.01xz.net/wiki/Module_name
+
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+    
+    mod_a instance_1(.in1(a), .in2(b), .in3(c), .in4(d), .out1(out1), .out2(out2));
+
+endmodule
