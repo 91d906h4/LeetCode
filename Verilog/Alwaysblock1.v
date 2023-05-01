518f76479d603d6879ae058a5251a6cd2e166896
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 00:10:35 +0900

    Create Alwaysblock1.v

---
+++ Verilog/Alwaysblock1.v
@@ -0,0 +1,13 @@
+// https://hdlbits.01xz.net/wiki/Alwaysblock1
+
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign = a & b;
+    always @(*) out_alwaysblock = a & b;
+
+endmodule
