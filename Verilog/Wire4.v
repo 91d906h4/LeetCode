a04cfd4ae416bdfc55dbe70b3f887a9a91d5cc8d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-01 12:40:24 +0900

    Create Wire4.v

---
+++ Verilog/Wire4.v
@@ -0,0 +1,12 @@
+// https://hdlbits.01xz.net/wiki/Wire4
+
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+
+    assign w = a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
+    
+endmodule
