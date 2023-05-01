e4b8bc966e3c08350aefc2b7b562d2be37fbb7ef
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-01 23:26:36 +0900

    Create Module shift.v

---
+++ Verilog/Module shift.v
@@ -0,0 +1,10 @@
+// https://hdlbits.01xz.net/wiki/Module_shift
+
+module top_module ( input clk, input d, output q );
+    wire w1, w2;
+
+    my_dff instance_1(clk, d, w1),
+    	     instance_2(clk, w1, w2),
+    	     instance_3(clk, w2, q);
+
+endmodule
