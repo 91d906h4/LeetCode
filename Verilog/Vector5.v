73912021a4a4a250127d28193ccc401088f42351
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-01 14:06:48 +0900

    Create Vector5.v

---
+++ Verilog/Vector5.v
@@ -0,0 +1,9 @@
+// https://hdlbits.01xz.net/wiki/Vector5
+
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+
+    assign out = ~ {{5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}}} ^ {{5{a, b, c, d, e}}};
+
+endmodule
