f964fde22df4d132150720efbc08971f830a2283
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 15:13:43 +0900

    Create Mux2to1v.v

---
+++ Verilog/Mux2to1v.v
@@ -0,0 +1,10 @@
+// https://hdlbits.01xz.net/wiki/Mux2to1v
+
+module top_module( 
+    input [99:0] a, b,
+    input sel,
+    output [99:0] out );
+    
+    assign out = (sel == 0) ? a : b;
+
+endmodule
