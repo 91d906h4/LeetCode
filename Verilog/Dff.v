e59a94fba8e0a3391c01b74f3f68cede3fbf5e9b
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 17:55:17 +0900

    Create Dff.v

---
+++ Verilog/Dff.v
@@ -0,0 +1,16 @@
+// https://hdlbits.01xz.net/wiki/Dff
+
+module top_module (
+    input clk,    // Clocks are used in sequential circuits
+    input d,
+    output reg q );//
+
+    // Use a clocked always block
+    //   copy d to q at every positive edge of clk
+    //   Clocked always blocks should use non-blocking assignments
+    
+    always @(posedge clk) begin
+        q <= d;
+    end
+
+endmodule
