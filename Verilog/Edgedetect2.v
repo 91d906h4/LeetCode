39da15d5864c6c63d955b12aa6af71868eab2366
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-03 00:32:36 +0900

    Create Edgedetect2.v

---
+++ Verilog/Edgedetect2.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    
+    reg [7:0] temp;
+    
+    always @(posedge clk) begin
+        temp <= in;
+    	anyedge <= in ^ temp;
+    end
+
+endmodule
