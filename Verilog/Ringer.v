f9c767dc23c8e1a564c71f8e10457f120062d0f6
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 14:07:08 +0900

    Create Ringer.v

---
+++ Verilog/Ringer.v
@@ -0,0 +1,13 @@
+// https://hdlbits.01xz.net/wiki/Ringer
+
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
+    
+    assign ringer = (vibrate_mode == 0 & ring == 1) ? 1 : 0;
+    assign motor = (vibrate_mode == 1 & ring == 1) ? 1 : 0;
+
+endmodule
