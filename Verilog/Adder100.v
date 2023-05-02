a25ece56faa759e1421c8f7421c36d1af694674a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 16:11:20 +0900

    Create Adder100.v

---
+++ Verilog/Adder100.v
@@ -0,0 +1,11 @@
+// https://hdlbits.01xz.net/wiki/Adder100
+
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output cout,
+    output [99:0] sum );
+    
+    assign {cout, sum} = a + b + cin;
+
+endmodule
