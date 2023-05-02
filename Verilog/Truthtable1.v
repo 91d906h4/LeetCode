920bc2ad11606f1758e8b573106c99093c7169b4
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 13:51:49 +0900

    Create Truthtable1.v

---
+++ Verilog/Truthtable1.v
@@ -0,0 +1,12 @@
+// https://hdlbits.01xz.net/wiki/Truthtable1
+
+module top_module( 
+    input x3,
+    input x2,
+    input x1,  // three inputs
+    output f   // one output
+);
+    
+    assign f = (~x3 & x2) | (x3 & x1);
+
+endmodule
