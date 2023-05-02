84eec9ee4b83c532e32f6ac7d5bea7d3b2d6e05a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-02 13:43:49 +0900

    Create m2014 q4g.v

---
+++ Verilog/m2014 q4g.v
@@ -0,0 +1,11 @@
+// https://hdlbits.01xz.net/wiki/Exams/m2014_q4g
+
+module top_module (
+    input in1,
+    input in2,
+    input in3,
+    output out);
+
+    assign out = ~(in1 ^ in2) ^ in3;
+
+endmodule
