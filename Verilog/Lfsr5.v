5ae49f0de4afcc986a6b92a491a9857129a8a18e
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-03 16:07:02 +0900

    Create Lfsr5.v

---
+++ Verilog/Lfsr5.v
@@ -0,0 +1,19 @@
+module top_module(
+    input clk,
+    input reset,    // Active-high synchronous reset to 5'h1
+    output [4:0] q
+);
+    
+    always @(posedge clk) begin
+        if (reset)
+            q <= 1;
+        else begin
+            q[0] <= q[1];
+        	  q[1] <= q[2];
+        	  q[2] <= q[3] ^ q[0];
+        	  q[3] <= q[4];
+        	  q[4] <= 0 ^ q[0];
+        end
+    end
+
+endmodule
