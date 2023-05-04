fc88297e59b18776dcad9308a15d48e74493e08c
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-04 14:21:42 +0900

    Create Fsm2.v

---
+++ Verilog/Fsm2.v
@@ -0,0 +1,69 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        case (state)
+        	OFF: next_state = j ? ON : OFF;
+            ON: next_state = k ? OFF : ON;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if (areset)
+            state <= OFF;
+        else
+            state <= next_state;
+    end
+
+    // Output logic
+    assign out = (state == OFF) ? OFF : ON;
+
+endmodule
+
+/*
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        if (state == OFF) begin
+            if (j)
+                next_state <= ON;
+            else
+                next_state <= state;
+        end
+        else begin
+            if (k)
+                next_state <= OFF;
+            else
+                next_state <= state;
+        end
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if (areset)
+            state <= OFF;
+        else
+            state <= next_state;
+    end
+
+    // Output logic
+    assign out = (state == OFF) ? OFF : ON;
+
+endmodule
+*/
