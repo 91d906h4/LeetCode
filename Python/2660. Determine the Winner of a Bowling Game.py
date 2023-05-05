5c35b0bf7a12f1e7884fd4008e60f7a425ec1e58
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-05 15:18:15 +0900

    Update 2660. Determine the Winner of a Bowling Game.py

--- Python/2660. Determine the Winner of a Bowling Game.py
+++ Python/2660. Determine the Winner of a Bowling Game.py
@@ -7,7 +7,6 @@ class Solution:
             elif i - 2 >= 0 and player1[i - 2] == 10: p1 += 2 * player1[i]
             else: p1 += player1[i]
 
-        for i in range(len(player2)):
             if i - 1 >= 0 and player2[i - 1] == 10: p2 += 2 * player2[i]
             elif i - 2 >= 0 and player2[i - 2] == 10: p2 += 2 * player2[i]
             else: p2 += player2[i]
