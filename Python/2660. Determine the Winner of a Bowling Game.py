d8d6afdca176becdd238f59d0842b923f783e42d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-05-05 15:17:16 +0900

    Create 2660. Determine the Winner of a Bowling Game.py

---
+++ Python/2660. Determine the Winner of a Bowling Game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isWinner(self, player1: List[int], player2: List[int]) -> int:
+        p1, p2 = 0, 0
+
+        for i in range(len(player1)):
+            if i - 1 >= 0 and player1[i - 1] == 10: p1 += 2 * player1[i]
+            elif i - 2 >= 0 and player1[i - 2] == 10: p1 += 2 * player1[i]
+            else: p1 += player1[i]
+
+        for i in range(len(player2)):
+            if i - 1 >= 0 and player2[i - 1] == 10: p2 += 2 * player2[i]
+            elif i - 2 >= 0 and player2[i - 2] == 10: p2 += 2 * player2[i]
+            else: p2 += player2[i]
+
+        if p1 == p2: return 0
+        return 1 if p1 > p2 else 2
