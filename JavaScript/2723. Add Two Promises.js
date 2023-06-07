12bef3e7c1458c2d3f3632921735b28f823f8787
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-06-07 20:26:55 +0900

    Create 2723. Add Two Promises.js

---
+++ JavaScript/2723. Add Two Promises.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let a = await promise1, b = await promise2;
+    return a + b;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
