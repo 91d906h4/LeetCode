109aa9934ec61d9851a349e70286cda140c3647a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-06-08 13:06:40 +0900

    Create 2677. Chunk Array.js

---
+++ JavaScript/2677. Chunk Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    let res = [], i = 0;
+
+    while (i < arr.length) {
+        res.push(arr.slice(i, i + size));
+        i += size;
+    }
+
+    return res;
+};
