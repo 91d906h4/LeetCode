7a91913cfe44e6f8bc6f2526a389bca21756d6b8
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-06-14 20:12:06 +0900

    Create 1663. Smallest String With A Given Numeric Value.py

---
+++ Python/1663. Smallest String With A Given Numeric Value.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        print(ord('z') - 96)
+        k -= n
+
+        temp = [1 for _ in range(n)]
+
+        while k > 0:
+            n -= 1
+            
+            if k >= 25:
+                temp[n] += 25
+                k -= 25
+            else:
+                temp[n] += k
+                k = 0
+
+        return "".join((chr(x + 96) for x in temp))
