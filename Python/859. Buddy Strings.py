1ccddb57f4f2f5b6a4f5e27568ed4a478556e2f4
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-07-03 15:31:34 +0900

    Update 859. Buddy Strings.py

--- Python/859. Buddy Strings.py
+++ Python/859. Buddy Strings.py
@@ -1,18 +1,27 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
         if len(s) != len(goal): return False
+
         temp = []
         for i, j in enumerate(s):
             if goal[i] != j:
                 temp.append(i)
+
+        # if len(temp) > 2: return False
+        # elif len(temp) == 2:
+        #     goal = list(goal)
+        #     goal[temp[0]], goal[temp[1]] = goal[temp[1]], goal[temp[0]]
+        #     goal = "".join(goal)
+        #     return s == goal
+        # else:
+        #     if s == goal:
+        #         return max(Counter(s).values()) >= 2
+        #     else:
+        #         return False
+
         if len(temp) > 2: return False
         elif len(temp) == 2:
-            goal = list(goal)
-            goal[temp[0]], goal[temp[1]] = goal[temp[1]], goal[temp[0]]
-            goal = "".join(goal)
-            return s == goal
+            return goal[temp[0]] == s[temp[1]] and goal[temp[1]] == s[temp[0]]
         else:
-            if s == goal:
-                return max(Counter(s).values()) >= 2
-            else:
-                return False
+            if goal == s: return max(Counter(s).values()) >= 2
+            else: return False
