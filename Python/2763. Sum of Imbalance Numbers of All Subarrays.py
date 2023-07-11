9c266cbf4130257fd5adef84153a9b2cf85d6893
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-07-11 20:42:49 +0900

    Update 2763. Sum of Imbalance Numbers of All Subarrays.py

--- Python/2763. Sum of Imbalance Numbers of All Subarrays.py
+++ Python/2763. Sum of Imbalance Numbers of All Subarrays.py
@@ -7,7 +7,7 @@ class Solution:
             s.add(nums[i])
 
             for j in range(i + 1, n):
-                if nums[j] in s: t += 0
+                if nums[j] in s: pass
                 elif nums[j] + 1 not in s and nums[j] - 1 not in s: t += 1
                 elif nums[j] + 1 in s and nums[j] - 1 in s: t -= 1
 
