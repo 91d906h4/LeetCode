d51b478d927ffe1cfe123387701912a27e24d5c4
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-07-12 21:55:00 +0900

    Update 1793. Maximum Score of a Good Subarray.cpp

--- C++/1793. Maximum Score of a Good Subarray.cpp
+++ C++/1793. Maximum Score of a Good Subarray.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        int i = k, j = k, n = nums.size(), res = nums[k], t = nums[k], m = 2;
+        int a, b, i = k, j = k, n = nums.size(), res = nums[k], t = nums[k], m = 2;
 
         while (i > 0 || j < n - 1) {
-            int a, b;
+            a = (i == 0) ? 0 : nums[i - 1];
+            b = (j == n - 1) ? 0 : nums[j + 1];
 
-            if (i == 0) a = 0;
-            else a = nums[i - 1];
-
-            if (j == n - 1) b = 0;
-            else b = nums[j + 1];
-
-            if (a < b) j++;
-            else i--;
+            a < b ? j++ : i--;
 
             t = min(t, min(nums[i], nums[j]));
             res = max(res, t * m++);
