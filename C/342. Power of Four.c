9595018183f0e85c0cb387e15efb2f1af7f95f42
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-07-14 21:14:50 +0900

    Update 342. Power of Four.c

--- C/342. Power of Four.c
+++ C/342. Power of Four.c
@@ -2,9 +2,9 @@ bool isPowerOfFour(int n){
     if (n == 1) return 1;
     if (n % 2 || n < 0) return 0;
 
-    for (long long  i = 1; i <= n; i *= 4) {
-        if (i == n) return 1;
-    }
+    long long i = 1;
 
-    return 0;
+    while (i < n) i *= 4;
+
+    return i == n;
 }
