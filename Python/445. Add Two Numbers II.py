1cbd9d575a56a3dee0a23e89f4c4a953f074cdb8
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-07-17 19:47:40 +0900

    Update 445. Add Two Numbers II.py

--- Python/445. Add Two Numbers II.py
+++ Python/445. Add Two Numbers II.py
@@ -7,16 +7,14 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         a, b = 0, 0
 
-        while 1:
+        while l1:
             a *= 10
             a += l1.val
-            if l1.next == None: break
             l1 = l1.next
 
-        while 1:
+        while l2:
             b *= 10
             b += l2.val
-            if l2.next == None: break
             l2 = l2.next
 
         a += b
