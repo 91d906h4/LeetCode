a62f33454711f901c5839ef04b1aee8fdd2562ac
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-07-26 20:20:44 +0900

    Create 1609. Even Odd Tree.py

---
+++ Python/1609. Even Odd Tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val % 2 == 0: return False
+
+        queue = deque([root])
+        odd, temp = True, 0
+
+        while queue:
+            n = len(queue)
+
+            for _ in range(n):
+                v = queue.popleft()
+                if v.left: queue.append(v.left)
+                if v.right: queue.append(v.right)
+                if v:
+                    if odd:
+                        if v.val % 2 == 0: return False
+                        if temp < v.val: temp = v.val
+                        else: return False
+                    else:
+                        if v.val % 2 == 1: return False
+                        if temp == 0: temp = float("inf")
+                        if temp > v.val: temp = v.val
+                        else: return False
+
+            odd = not odd
+            temp = 0
+                
+        return True
