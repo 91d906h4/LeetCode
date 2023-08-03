fc9d04521544ab3d438e425bcb5986c69d8cf96a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-08-03 21:35:26 +0900

    Create 113. Path Sum II.py

---
+++ Python/113. Path Sum II.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    s = 0
+    res = []
+    path = []
+    targetSum = 0
+
+    def dfs(self, node: Optional[TreeNode]) -> None:
+        if not node.left and not node.right:
+            if self.s + node.val == self.targetSum:
+                self.res.append(self.path + [node.val])
+
+            return
+
+        self.path.append(node.val)
+        self.s += node.val
+
+        if node.left: self.dfs(node.left)
+        if node.right: self.dfs(node.right)
+
+        self.path.pop()
+        self.s -= node.val
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root: return []
+
+        self.s = 0
+        self.res = []
+        self.path = []
+        self.targetSum = targetSum
+        self.dfs(root)
+        return self.res
