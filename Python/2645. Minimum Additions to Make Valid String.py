7141b10cbf3dcd51025aea4401fd34631c1252c1
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-08-09 12:18:46 +0900

    Update 2645. Minimum Additions to Make Valid String.py

--- Python/2645. Minimum Additions to Make Valid String.py
+++ Python/2645. Minimum Additions to Make Valid String.py
@@ -4,7 +4,9 @@ class Solution:
         res = 0
 
         while ps < len(word):
-            if abc[pa] == word[ps]: pa = (pa + 1) % 3; ps += 1
-            else: res += 1; pa = (pa + 1) % 3
+            if abc[pa] == word[ps]: ps += 1
+            else: res += 1
+
+            pa = (pa + 1) % 3
 
         return res + (3 - pa if pa != 0 else 0)
