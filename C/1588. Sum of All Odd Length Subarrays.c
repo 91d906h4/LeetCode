60441f4d1f41f19b56b702a0c0406020981f2244
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-08-09 17:59:50 +0900

    Create 1588. Sum of All Odd Length Subarrays.c

---
+++ C/1588. Sum of All Odd Length Subarrays.c
@@ -0,0 +1,8 @@
+int sumOddLengthSubarrays(int* arr, int arrSize){
+    int res = 0;
+
+    for (int i = 0; i < arrSize; i++)
+        res += ((i + 1) * (arrSize - i) + 1) / 2 * *(arr + i);
+
+    return res;
+}
