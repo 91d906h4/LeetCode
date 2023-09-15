ec602514c20c8b3e530e11c1841cb78ba612892f
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2023-09-15 19:15:37 +0900

    Update 389. Find the Difference.py

--- Python/389. Find the Difference.py
+++ Python/389. Find the Difference.py
@@ -1,5 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        for i in s:
-            t = t.replace(i, "", 1)
-        return t
+        # for i in s:
+        #     t = t.replace(i, "", 1)
+        # return t
+
+        for i, j in zip_longest(sorted(s), sorted(t)):
+            if i != j:
+                if i and i not in t: return i
+                return j
