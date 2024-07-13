9471fdab8950f401004bc48f80c7a9a604976d35
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2024-07-13 21:47:02 +0900

    Update 2884. Modify Columns.py

--- Python/2884. Modify Columns.py
+++ Python/2884. Modify Columns.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    return employees.mul({"name": 1, "salary": 2})
+    # return employees.mul({"name": 1, "salary": 2})
+
+    employees["salary"] *= 2
+    return employees
