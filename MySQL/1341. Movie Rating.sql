1aebb283fa5ed2ed3bc8ec9a56aa4ba7b809e81a
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2024-07-17 16:42:35 +0900

    Create 1341. Movie Rating.sql

---
+++ MySQL/1341. Movie Rating.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+(
+    SELECT name AS results
+    FROM MovieRating
+    JOIN Users USING(user_id)
+    GROUP BY user_id
+    ORDER BY COUNT(user_id) DESC, name
+    LIMIT 1
+) UNION ALL (
+    SELECT title AS results
+    FROM MovieRating
+    JOIN Movies USING(movie_id)
+    WHERE EXTRACT(YEAR_MONTH FROM created_at) = 202002
+    GROUP BY movie_id
+    ORDER BY AVG(rating) DESC, title
+    LIMIT 1
+)
