1a4c58f6fc6a91349ef8cd9255f85e5988d72457
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2024-07-30 18:16:28 +0900

    Create 2785. Sort Vowels in a String.c

---
+++ C/2785. Sort Vowels in a String.c
@@ -0,0 +1,44 @@
+compare (a, b)
+char* a;
+char* b;
+{
+    return (*a == *b) ? 0 : strcmp(a, b);
+}
+
+isVowel (a)
+char a;
+{
+    if (
+        a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U' ||
+        a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u'
+    ) return true;
+
+    return false;
+}
+
+char * sortVowels(char * s){
+    int s_len = strlen(s);
+    int v_count = 0;
+    char *temp = (char *)malloc(sizeof(char) * s_len);
+
+    for (int i = 0; i < s_len; i++){
+        if (isVowel(i[s]))
+            (v_count++)[temp] = i[s];
+        }
+
+    // Return if no or one vowel.
+    if (v_count <= 1)
+        return s;
+
+    qsort(temp, v_count, sizeof(char), compare);
+
+    v_count = 0;
+
+    for (int i = 0; i < s_len; i++)
+        if (isVowel(i[s]))
+            i[s] = (v_count++)[temp];
+
+    free(temp);
+
+    return s;
+}
