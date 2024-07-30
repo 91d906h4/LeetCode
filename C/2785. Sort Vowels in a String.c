00ea77b3381f37425f620aad806612bf9cf49961
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2024-07-30 18:17:03 +0900

    Update 2785. Sort Vowels in a String.c

--- C/2785. Sort Vowels in a String.c
+++ C/2785. Sort Vowels in a String.c
@@ -21,10 +21,9 @@ char * sortVowels(char * s){
     int v_count = 0;
     char *temp = (char *)malloc(sizeof(char) * s_len);
 
-    for (int i = 0; i < s_len; i++){
+    for (int i = 0; i < s_len; i++)
         if (isVowel(i[s]))
             (v_count++)[temp] = i[s];
-        }
 
     // Return if no or one vowel.
     if (v_count <= 1)
