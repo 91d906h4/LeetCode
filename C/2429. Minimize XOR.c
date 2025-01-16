5b9de4946b4c2f7fadbfa66214a93573da526ef6
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2025-01-17 03:11:05 +0900

    Update 2429. Minimize XOR.c

--- C/2429. Minimize XOR.c
+++ C/2429. Minimize XOR.c
@@ -1,16 +1,16 @@
+int count_bits(int num) {
+    int bits = 0;
+
+    while (num) {
+        bits += num % 2;
+        num >>= 1;
+    }
+
+    return bits;
+}
+
 int minimizeXor(int num1, int num2) {
-    int num1_bits = 0, num2_bits = 0,
-        num1_temp = num1, num2_temp = num2;
-
-    while (num1_temp) {
-        num1_bits += num1_temp % 2;
-        num1_temp >>= 1;
-    }
-
-    while (num2_temp) {
-        num2_bits += num2_temp % 2;
-        num2_temp >>= 1;
-    }
+    int num1_bits = count_bits(num1), num2_bits = count_bits(num2);
 
     int diff = num1_bits - num2_bits;
 
