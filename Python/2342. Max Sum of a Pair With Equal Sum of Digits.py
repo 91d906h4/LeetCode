303833dd98e5349be8bba4090432b318aed4573b
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2025-02-12 14:26:42 +0900

    Update 2342. Max Sum of a Pair With Equal Sum of Digits.py

--- Python/2342. Max Sum of a Pair With Equal Sum of Digits.py
+++ Python/2342. Max Sum of a Pair With Equal Sum of Digits.py
@@ -1,11 +1,39 @@
 class Solution:
-    def maximumSum(self, nums: List[int]) -> int:
-        dict_ = defaultdict(list)
-        for i in nums:
-            dict_[sum(map(int, str(i)))].append(i)
+    @staticmethod
+    def digsum(n: int) -> int:
+        r = 0
 
-        m = 0
-        for i in dict_.values():
-            i.sort()
-            if len(i) >= 2: m = max(m, i[-1] + i[-2])
-        return m if m != 0 else -1
+        while n:
+            r += n % 10
+            n //= 10
+
+        return r
+
+    def maximumSum(self, nums: List[int]) -> int:
+        # dict_ = defaultdict(list)
+        # for i in nums:
+        #     dict_[sum(map(int, str(i)))].append(i)
+
+        # m = 0
+        # for i in dict_.values():
+        #     i.sort()
+        #     if len(i) >= 2: m = max(m, i[-1] + i[-2])
+        # return m if m != 0 else -1
+
+        s2i = {}
+        result = -1
+
+        for i, n in enumerate(nums):
+            s = self.digsum(n)
+
+            if s not in s2i: s2i[s] = [i]
+            else: s2i[s].append(i)
+
+        for s, i in s2i.items():
+            if len(i) <= 1: continue
+
+            i = sorted(i, key=lambda i: nums[i], reverse=True)
+
+            result = max(result, nums[i[0]] + nums[i[1]])
+
+        return result
