3233cbf717a4f3bdb40105098dbaea01897aed0d
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2025-02-20 12:11:19 +0900

    Update 1980. Find Unique Binary String.py

--- Python/1980. Find Unique Binary String.py
+++ Python/1980. Find Unique Binary String.py
@@ -5,6 +5,6 @@ class Solution:
         for n in nums:
             table[int(n, 2)] = True
 
-        for i in range(2 ** 16):
+        for i in range(17):
             if i not in table:
                 return "{0:b}".format(i).zfill(len(nums))
