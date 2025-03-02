aba690ed4427823c869c3424bce50477c24c0dde
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2025-03-02 23:04:02 +0900

    Create 1. Two Sum.php

---
+++ PHP/1. Two Sum.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $arr = array();
+
+        for ($i = 0; $i < count($nums); $i++) {
+            if (in_array($target - $nums[$i], $arr))
+                return array($i, array_search($target - $nums[$i], $arr));
+            
+            $arr[$i] = $nums[$i];
+        }
+
+        return array(0, 0);
+    }
+}
